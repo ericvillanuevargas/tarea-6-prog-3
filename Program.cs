11515920887d47d61b2b9306be8f31971862f466
alex010620 <66653033+alex010620@users.noreply.github.com>
2021-07-04 19:00:57 -0400

    commit hecho por alex

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tarea_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo nosotros somos el equipo the fast code!");
+        }
+    }
+}
